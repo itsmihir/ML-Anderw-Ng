f3cb194c291db80448231dd0ab394722482eb7e9
itsmihir <inmihir11@gmail.com>
2021-12-31 19:06:41 +0530

    week 5

--- Assignment 3/predict.m
+++ Assignment 3/predict.m
@@ -8,7 +8,7 @@ m = size(X, 1);
 num_labels = size(Theta2, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+p = zeros(m, 1);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -21,14 +21,28 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
+X = X';
+a1 = [ones(1,m); X];
 
+z2 = Theta1 * a1;
+a2 = sigmoid(z2);
 
+a2 = [ones(1,m); a2];
 
+z3 = Theta2 * a2;
+a3 = sigmoid(z3);
 
+[maxm,p] = max(a3);
+p = p';
 
-
-
-
+% Dim
+% s1 = units at level 1, s2 = units at level 2, s3 = units at level 3
+% X = m * s1
+% Theta1 = s2 * (s1 + 1)
+% Theta2 = s3 * (s2 + 1)
+% a1 = (s1 + 1) * m;
+% a2 = (s2 + 1) * m
+% a3 = s3 * m
 % =========================================================================
 
 
--- Assignment 3/predictOneVsAll.m
+++ Assignment 3/predictOneVsAll.m
@@ -12,7 +12,7 @@ m = size(X, 1);
 num_labels = size(all_theta, 1);
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+p = zeros(m, 1);
 
 % Add ones to the X data matrix
 X = [ones(m, 1) X];
@@ -30,12 +30,14 @@ X = [ones(m, 1) X];
 %       for each row.
 %       
 
+p = X * all_theta';
+[maxm,p] = max(p');
 
 
-
-
-
-
+% Dim
+% all_theta = num_labels * (n + 1);
+% X = m * (n + 1);
+% p = m * 1
 % =========================================================================
 
 
